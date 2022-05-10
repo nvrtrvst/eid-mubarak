d7f0730999c9682f38082d886b1e5b67be44c533
Reza Muhammad A <38930001+nvrtrvst@users.noreply.github.com>
2022-05-10 15:55:53 +0700

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        almarai: ['Almarai'],
+        poppins: ['Poppins'],
+      },
+    },
   },
   plugins: [],
-}
+};
